1d23f4e5e20ab6663dd08cb1748f8b840daaacda
Phillip Schallmayer <phillip.schallmayer@gmx.at>
2014-12-09 22:51:50 +0100

    added base for user controller

---
+++ controller/user.js
@@ -0,0 +1,25 @@
+var User = require('../models/User');
+
+exports.register = function(req, res, next) {
+    // do sth.
+};
+
+exports.delete = function(req, res, next) {
+    // do sth.
+};
+
+exports.update = function(req, res, next) {
+    // do sth.
+};
+
+exports.getFriends = function(req, res, next) {
+    // do sth.
+};
+
+exports.addFriend = function(req, res, next) {
+    // do sth.
+};
+
+exports.removeFriend = function(req, res, next) {
+    // do sth.
+};
