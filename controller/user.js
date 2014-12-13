6124e64c2eb3513c1487c3dc28ac78686c0ae3ed
Phillip Schallmayer <phillip.schallmayer@gmx.at>
2014-12-13 13:54:24 +0100

    Added async module, added implementation of getFriends and addFriend in user controller

--- controller/user.js
+++ controller/user.js
@@ -1,4 +1,5 @@
 var User = require('../models/User');
+var async = require('async');
 
 exports.register = function(req, res, next) {
     // do sth.
@@ -9,17 +10,17 @@ exports.register = function(req, res, next) {
 
     User.findOne({ email: data.email }, function(err, user) {
         if (err) {
-            // @TODO: log error
-            console.log(err);
+            return next(err);
         }
 
-        // @TODO: Handle user already exists
-        if (!user) {
+        if (user) {
+            return next(new Error('User with email ' + data.email) + ' already exists');
+        } else {
             var newUser = new User({
                 firstname: data.firstname,
                 lastname: data.lastname,
                 email: data.email,
-                password: data.firstPassword,
+                password: data.firstPassword
             });
 
             newUser.save();
@@ -36,11 +37,88 @@ exports.update = function(req, res, next) {
 };
 
 exports.getFriends = function(req, res, next) {
-    // do sth.
+    var data = req.body;
+
+    // TODO: move User.findOne with friends to a separate module to reduce code
+    User.findOne({ _id: data.id })
+        .populate({
+            path: 'friends',
+            match: { deleted: false }
+        })
+        .exec(function (err, user) {
+            if (err) {
+                return next(err);
+            }
+
+            if (user) {
+                return res.json({
+                    message: null,
+                    friends: user.friends
+                });
+            } else {
+                return next(new Error('Unable to find user with id ' + data.id));
+            }
+        });
 };
 
 exports.addFriend = function(req, res, next) {
-    // do sth.
+    var data = req.body;
+    var currentUser = req.user;
+
+    if (currentUser._id === data.id || currentUser.admin) {
+        return next(new Error('Id parameter has to be id of your user or id of an admin user'), null);
+    }
+
+    async.parallel([
+        function(callback) {
+            // TODO: move User.findOne with friends to a separate module to reduce code
+            User.findOne({ _id: data.id })
+                .populate({
+                    path: 'friends',
+                    match: { deleted: false }
+                })
+                .exec(function (err, user) {
+                    if (err) {
+                        return callback(err, null);
+                    }
+
+                    if (user) {
+                        return callback(null, user);
+                    } else {
+                        return callback(new Error('Unable to find user with id ' + data.id), null);
+                    }
+                });
+        },
+        function(callback) {
+            // TODO: move User.findOne with friends to a separate module to reduce code
+            User.findOne({ _id: data.friendId })
+                .populate({
+                    path: 'friends',
+                    match: { deleted: false }
+                })
+                .exec(function (err, user) {
+                    if (err) {
+                        return callback(err, null);
+                    }
+
+                    if (user) {
+                        return callback(null, user);
+                    } else {
+                        return callback(new Error('Unable to find user with id ' + data.id), null);
+                    }
+                });
+        }
+    ],
+    function(err, users) {
+        if (err) {
+            return next(err);
+        }
+
+        users[0].friends.push(users[1]);
+        users[1].friends.push(users[0]);
+        users[0].save();
+        users[1].save();
+    });
 };
 
 exports.removeFriend = function(req, res, next) {
